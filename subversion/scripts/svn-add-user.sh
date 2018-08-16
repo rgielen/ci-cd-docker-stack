c02d641b967e485b49853987e6ee36f664fdee5a
Rene Gielen <rene.gielen@gmail.com>
2018-08-16 15:19:49 +0200

    delete now unneeded subversion custom build files

--- subversion/scripts/svn-add-user.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-: ${1?"Usage: $0 USER_NAME"}
-
-htpasswd -m ${SVN_PASSWD} $1
-/usr/sbin/apache2ctl restart
--- subversion/scripts/svn-edit-auth.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -e "${SVN_AUTHFILE}" ]; then
-    cp /dav_svn.authz.sample ${SVN_AUTHFILE}
-fi
-
-nano ${SVN_AUTHFILE}
-/usr/sbin/apache2ctl restart
